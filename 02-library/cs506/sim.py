c81bea24fd6ab02e6c05226895e1c76bec283d50
Yichuan Ma <yichuanma@dhcp-wifi-8021x-155-41-65-125.bu.edu>
2021-09-30 12:40:42 -0400

    similarities and distances

--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -5,12 +5,20 @@ def euclidean_dist(x, y):
     return res**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    return sum(abs(x_i - y[i]) for i, x_i in enumerate(x))
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    if len(x) == 0 and len(y) == 0: raise ValueError("lengths must not be zero")
+    x_set = set(x)
+    y_set = set(y)
+    return 1 - (len(x_set.intersection(y_set)) / len(x_set.union(y_set)))
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    if len(x) == 0 or len(y) == 0: raise ValueError("lengths must not be zero")
+    if len(x) != len(y): raise ValueError("lengths must be equal")
+    prod = sum(x_i * y[i] for i, x_i in enumerate(x))
+    return prod / (norm(x) * norm(y))
 
 # Feel free to add more
+def norm(v):
+    return sum(v_i ** 2 for v_i in v) ** (1/2)
